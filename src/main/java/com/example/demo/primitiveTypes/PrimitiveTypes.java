7ef33e3dd804cd85ef79ed5c25ba62193e672fe4
Paulius <paulius.mac92@gmail.com>
2022-08-18 17:15:14 +0300

    Master: adding packages for learning.

---
+++ src/main/java/com/example/demo/primitiveTypes/PrimitiveTypes.java
@@ -0,0 +1,14 @@
+package com.example.demo.primitiveTypes;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PrimitiveTypes {
+
+}
